c45624a03f06aabcbd8091e0b0fe4a9901da1f8b
MrWakeCZ <joseff.kral@gmail.com>
2018-01-08 00:20:31 +0100

    Update configu

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -18,7 +18,7 @@ const users = [
 const siteConfig = {
   title: 'Test Site' /* title for your website */,
   tagline: 'A website for testing',
-  url: 'https://facebook.github.io' /* your website url */,
+  url: 'https://mrwakecz.github.io/' /* your website url */,
   baseUrl: '/' /* base url for your project */,
   projectName: 'Minecraft-help',
   headerLinks: [
